f15613187029b6b21dd074213df1ee89c904e099
PG3447 <134543419+PG3447@users.noreply.github.com>
2026-06-03 00:11:23 +0200

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -337,6 +337,7 @@ void processCameraInput(ECS& ecs, CameraComponent& cam, TransformComponent& tran
         dir = glm::normalize(dir);
         transform.position += dir * MovementSpeed * deltaTime;
         transform.isDirty   = true;
+        cam.dirty = true;
     }
 }
 
@@ -1495,11 +1496,11 @@ void end_frame()
     glfwSwapBuffers(window);
 }
 
+float lookDeadzone = 0.0f;
+
 void processCameraGamepad(ECS& ecs, CameraComponent& cam, TransformComponent& transform, int gamepad_id)
 {
     const auto& hid = ecs.GetSystem<HID>();
-
-    const float lookDeadzone = 0.01f;
 
     float lx = hid->get_gamepad_axis(GLFW_GAMEPAD_AXIS_LEFT_X, gamepad_id);
     float ly = hid->get_gamepad_axis(GLFW_GAMEPAD_AXIS_LEFT_Y, gamepad_id);
@@ -1525,8 +1526,12 @@ void processCameraGamepad(ECS& ecs, CameraComponent& cam, TransformComponent& tr
     float rx = hid->get_gamepad_axis(GLFW_GAMEPAD_AXIS_RIGHT_X, gamepad_id);
     float ry = hid->get_gamepad_axis(GLFW_GAMEPAD_AXIS_RIGHT_Y, gamepad_id);
 
-    if (glm::abs(rx) < lookDeadzone && glm::abs(ry) < lookDeadzone)
+
+    if (lookDeadzone <= 0.01f)
+        lookDeadzone += 0.0005f;
+    else if (glm::abs(rx) < lookDeadzone && glm::abs(ry) < lookDeadzone)
         return;
+
 
     const float sensitivity = 600.0f;
     CameraHelper::ProcessMouseMovement(cam, transform,
