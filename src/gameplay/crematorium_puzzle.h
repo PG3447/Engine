1804edb57238eabd47ad26d070a6e985e3eb5298
minikont <syskamateusz07@gmail.com>
2026-06-25 00:12:03 +0200

    krem zagadka maly fix

--- src/gameplay/crematorium_puzzle.h
+++ src/gameplay/crematorium_puzzle.h
@@ -41,8 +41,8 @@ public:
 
     std::vector<std::vector<int>> configLeftWall = {
         {4, 4, 3, 1},
-        {2, 3, 2, 1},
-        {4, 4, 0, 2},
+        {2, 3, 0, 1},
+        {4, 4, 1, 2},
         {1, 4, 2, 1}
     };
 
