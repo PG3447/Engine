fac85aa1d6d28fc5f144d6a031040b1903b310e2
PG3447 <134543419+PG3447@users.noreply.github.com>
2026-09-11 19:42:12 +0200

    Revert "testy"
    This reverts commit acc0fefa305fa94aa7c5623b4d1b202088987c92.

--- src/main.cpp
+++ src/main.cpp
@@ -371,13 +371,6 @@ int main(int, char**)
     end_frame();
     //menu->GetECS().AddExistingSystem(scena1->GetECS().GetSystem<RenderSystem>());
 
-    GLenum err = glGetError();
-    if (err != GL_NO_ERROR)
-    {
-        printf("OpenGL error: 0x%X\n", err);
-        spdlog::error("error");
-    }
-
     postacGraczaCzerw = std::make_unique<Prefab>("res/models/postac_akcje_czerw.glb");
     postacGraczaZiel = std::make_unique<Prefab>("res/models/postac_akcje_ziel.glb");
     groundModel = std::make_unique<Prefab>("res/models/podloze.glb");
